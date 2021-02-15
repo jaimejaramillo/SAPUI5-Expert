2f83a8d65b6842acb5bbb195d675c36f0c6db3d7
jaimejaramillo <jaime.jaramillo@outlook.com>
2021-02-15 17:50:14 +0000

    05 Estructura Proyectos SAPUI5 02

--- Invoices/SAPUI5/webapp/controller/App.controller.js
+++ Invoices/SAPUI5/webapp/controller/App.controller.js
@@ -7,7 +7,6 @@ sap.ui.define([
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      * @param {typeof sap.m.MessageToast} MessageToast 
-     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel 
      */
     function (Controller, MessageToast, Models, ResourceModel) {
         "use strict";
@@ -16,14 +15,6 @@ sap.ui.define([
 
             onInit: function () {
 
-            },
-
-            onShowHello: function () {
-                //read text from i18n
-                var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMsg = oBundle.getText("helloMessage", [sRecipient]);
-                MessageToast.show(sMsg);
             }
 
         });
---
+++ Invoices/SAPUI5/webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,32 @@
+// @ts-nocheck
+//ts-nocheck
+/* eslint-disable no-undef */
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+],
+    /**
+     * 
+     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     * @param {typeof sap.m.MessageToast} MessageToast 
+     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel 
+     */
+    function (Controller, MessageToast) {
+        "use strict";
+
+        return Controller.extend("ns.SAPUI5.controller.HelloPanel", {
+
+            onInit: function () {
+
+            },
+
+            onShowHello: function () {
+                //read text from i18n
+                var oBundle = this.getView().getModel("i18n").getResourceBundle();
+                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+                var sMsg = oBundle.getText("helloMessage", [sRecipient]);
+                MessageToast.show(sMsg);
+            }
+        });
+
+    });
