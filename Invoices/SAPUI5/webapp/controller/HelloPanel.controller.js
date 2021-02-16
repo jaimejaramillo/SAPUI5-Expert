3436961b57a34fe5bbeb4263284a74e72398f5bf
jaimejaramillo <jaime.jaramillo@outlook.com>
2021-02-16 01:08:35 +0000

    05 Estructura Proyectos SAPUI5 03

--- Invoices/SAPUI5/webapp/controller/HelloPanel.controller.js
+++ Invoices/SAPUI5/webapp/controller/HelloPanel.controller.js
@@ -3,15 +3,17 @@
 /* eslint-disable no-undef */
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
 ],
     /**
      * 
      * @param {typeof sap.ui.core.mvc.Controller} Controller 
      * @param {typeof sap.m.MessageToast} MessageToast 
      * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel 
+     * @param {typeof sap.ui.core.Fragment} Fragment
      */
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Fragment) {
         "use strict";
 
         return Controller.extend("ns.SAPUI5.controller.HelloPanel", {
@@ -26,6 +28,27 @@ sap.ui.define([
                 var sRecipient = this.getView().getModel().getProperty("/recipient/name");
                 var sMsg = oBundle.getText("helloMessage", [sRecipient]);
                 MessageToast.show(sMsg);
+            },
+
+            onOpenDialog: function () {
+
+                const oView = this.getView();
+
+                if (!this.byId("helloDialog")) {
+                    Fragment.load({
+                        id: oView.getId(),
+                        name: "ns.SAPUI5.view.HelloDialog",
+                        controller: this
+                    }).then(function (oDialog) {
+                        oView.addDependent(oDialog);
+                        oDialog.open();
+                    });
+                } else {
+                    this.byId("helloDialog").open();
+                }
+            },
+            onCloseDialog: function () {
+                this.byId("helloDialog").close();
             }
         });
 
