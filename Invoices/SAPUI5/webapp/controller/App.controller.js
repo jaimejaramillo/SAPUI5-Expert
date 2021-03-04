a2b2eff85d48ecd642b2be217472c22840a17631
jaimejaramillo <jaime.jaramillo@outlook.com>
2021-03-04 21:20:41 +0000

    05 Estructura Proyectos SAPUI5 06

---
+++ Invoices/SAPUI5/webapp/controller/App.controller.js
@@ -0,0 +1,24 @@
+// @ts-nocheck
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * 
+     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     * @param {typeof sap.m.MessageToast} MessageToast 
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("ns.SAPUI5.controller.App", {
+
+            onInit: function () {
+
+            },
+            onOpenDialogHeader: function(){
+                 this.getOwnerComponent().openHelloDialog();
+            }
+
+        });
+
+    });
---
+++ Invoices/SAPUI5/webapp/test/unit/model/InvoicesFormatter.js
@@ -0,0 +1,47 @@
+// @ts-nocheck
+/* eslint-disable no-undef */
+sap.ui.define([
+    "ns/SAPUI5/model/InvoicesFormatter",
+    "sap/ui/model/resource/ResourceModel"
+],
+    /** 
+     * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
+    */
+
+    function (InvoicesFormatter, ResourceModel) {
+
+        QUnit.module("Qinvoices Status", {
+
+            beforeEach: function () {
+                this._oResourceModel = new ResourceModel({
+                    bundleUrl: sap.ui.require.toUrl("ns/SAPUI5") + "/i18n/i18n.properties"
+                });
+            },
+
+            afterEach: function () {
+                this._oResourceModel.destroy();
+            }
+        });
+
+        QUnit.test("Should return the Invoices Status", function(assert){
+
+            let oModel = this.stub();
+            oModel.withArgs("i18n").returns(this._oResourceModel);
+
+            let oViewStub = {
+                getModel : oModel
+            };
+
+            let oControllerStub = {
+                getView : this.stub().returns(oViewStub)
+            };
+
+            let fnIsolatedFormatter = InvoicesFormatter.invoiceStatus.bind(oControllerStub);
+
+            //Assert
+            assert.strictEqual(fnIsolatedFormatter("A") , "New", "The invoices status for A is correct" );
+            assert.strictEqual(fnIsolatedFormatter("B") , "In Progress", "The invoices status for B is correct" );
+            assert.strictEqual(fnIsolatedFormatter("C") , "Done", "The invoices status for C is correct" );
+        });
+
+    });
