27a5068ff4a79a4c29aeddc2b69e62c5b303aa1f
jaimejaramillo <jaime.jaramillo@outlook.com>
2021-03-16 21:32:29 +0000

    05 Estructura Proyectos SAPUI5 08

---
+++ Invoices/SAPUI5/webapp/controller/Details.controller.js
@@ -0,0 +1,21 @@
+// @ts-nocheck
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * 
+     * @param {typeof sap.ui.core.mvc.Controller} Controller 
+     * @param {typeof sap.m.MessageToast} MessageToast 
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("ns.SAPUI5.controller.Details", {
+
+            onInit: function () {
+
+            }
+
+        });
+
+    });
